0c587f5f6c3f787186531985c65ec146616a2efa
Ryo-rom <r0567280@gmail.com>
2026-09-05 16:16:14 +0900

    Add calculate button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { TimeInput } from "./components/TimeInput";
+import { TimeDuration } from "./utilities/TimeDuration";
 import { useState } from "react";
 type Term = { calType: string; time: string };
 
 function App() {
   const [terms, setTerms] = useState<Term[]>([{ calType: "+", time: "" }]);
-
+  const [cal, setCal] = useState<boolean>(false);
+  const [result, setResult] = useState<TimeDuration>(new TimeDuration(0, 0, 0));
   const handleAdd = () => {
     setTerms((prev) => [...prev, { calType: "+", time: "" }]);
   };
@@ -16,8 +18,25 @@ function App() {
     );
   };
 
+  const handleCal = () => {
+    setCal(true);
+    const total = terms.reduce(
+      (acc, term) => {
+        const value = TimeDuration.fromString(term.time);
+        return term.calType === "+" ? acc.add(value) : acc.subtract(value);
+      },
+      new TimeDuration(0, 0, 0),
+    );
+
+    setResult(total);
+  };
+
   return (
     <>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <label htmlFor="result">Result: </label>
+        {cal && <span className="result">{result.toString()}</span>}
+      </div>
       {terms.map((term, i) => (
         <TimeInput
           key={i}
@@ -27,7 +46,10 @@ function App() {
           onTimeChange={(v) => updateTerm(i, { time: v })}
         />
       ))}
-      <button onClick={handleAdd}>Add Term</button>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <button onClick={handleAdd}>Add Term</button>
+        <button onClick={handleCal}>Calculate</button>
+      </div>
     </>
   );
 }
